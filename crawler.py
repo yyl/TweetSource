78a004fa703316fafe88997a9c2bde88be4720b1
Yulong <yyl.dev@gmail.com>
2014-04-17 14:49:20 -0400

    crawler updated

--- crawler.py
+++ crawler.py
@@ -10,6 +10,11 @@ import sqlite3 as lite
 ## self import
 from secrets import *
 
+'''
+A script to collect tweets data using Twitter streaming API
+usage: python script.py amount_of_tweets_needed
+'''
+
 ## helper functions
 def cleanSource(source):
     if source == 'web':
@@ -21,10 +26,11 @@ def cleanSource(source):
         else:
             return source
 
+# urls and parameters used to query Twitter API
 STREAM_URL = 'https://stream.twitter.com/1.1/statuses/sample.json'
 STREAM_KEYS = {'language':'en', 'filter_level':'medium'}
-SEARCH_URL = 'https://api.twitter.com/1.1/search/tweets.json'
 
+# http request object
 oauth = OAuth1Session(APP_KEY, client_secret=APP_SECRET,
                           resource_owner_key=ACCESS_TOKEN,
                           resource_owner_secret=ACCESS_TOKEN_SECRET)
@@ -33,13 +39,14 @@ tstream = oauth.get(STREAM_URL, stream=True, params=STREAM_KEYS)
 count = 0
 with lite.connect("tweets.db") as conn:
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS cleaned (id INTEGER PRIMARY KEY, screen_name TEXT NOT NULL, content TEXT NOT NULL, source TEXT NOT NULL, created_at TEXT NOT NULL, sensitive TEXT NOT NULL);")
-    cursor.execute("CREATE TABLE IF NOT EXISTS spams (id INTEGER PRIMARY KEY, screen_name TEXT NOT NULL, content TEXT NOT NULL, source TEXT NOT NULL, created_at TEXT NOT NULL, sensitive TEXT NOT NULL);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS cleaned (id INTEGER PRIMARY KEY, screen_name TEXT NOT NULL, content TEXT NOT NULL, source TEXT NOT NULL, created_at TEXT NOT NULL, coordinates INTEGER NOT NULL, urls INTEGER NOT NULL);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS spam (id INTEGER PRIMARY KEY, screen_name TEXT NOT NULL, content TEXT NOT NULL, source TEXT NOT NULL, created_at TEXT NOT NULL, coordinates INTEGER NOT NULL, urls INTEGER NOT NULL);")
     for line in tstream.iter_lines():
         if line:
             tweet = json.loads(line)
             text = tweet.get(u'text', False)
             source = tweet.get(u'source', False)
+            # ignore the tweet if either source or text does not exist
             if source and text:
                 user = tweet.get(u'user', False)
                 if user:
@@ -48,17 +55,28 @@ with lite.connect("tweets.db") as conn:
                     dtweet.append(text)
                     dtweet.append(cleanSource(source))
                     dtweet.append(tweet.get(u'created_at', ''))
-                    dtweet.append(tweet.get(u'possibly_sensitive', False))
+                    # see if the tweet has location info
+                    if tweet.get(u'coordinates', False):
+                        dtweet.append(1)
+                    else:
+                        dtweet.append(0)
+                    # see if the tweet has links
+                    if tweet.get(u'entities', {}).get(u'urls', False):
+                        dtweet.append(1)
+                    else:
+                        dtweet.append(0)
+                    #dtweet.append(tweet.get(u'possibly_sensitive', False))
                     # naive logic for bot/spam/retweet detection
                     if tweet.get(u'possibly_sensitive', False) or \
                         user.get(u'friends_count', 0) < 10 or \
                         user.get(u'followers_count', 0) < 10 or \
                         tweet.get(u'retweeted_status', False) or \
                         'RT' in tweet.get(u'text', False):
-                        cursor.execute("INSERT INTO spams (screen_name,content,source,created_at,sensitive) VALUES (?,?,?,?,?)", dtweet)
+                        cursor.execute("INSERT INTO spam (screen_name,content,source,created_at,coordinates,urls) VALUES (?,?,?,?,?,?)", dtweet)
                     else:
-                        cursor.execute("INSERT INTO cleaned (screen_name,content,source,created_at,sensitive) VALUES (?,?,?,?,?)", dtweet)
+                        cursor.execute("INSERT INTO cleaned (screen_name,content,source,created_at,coordinates,urls) VALUES (?,?,?,?,?,?)", dtweet)
             count += 1
+        # periodic log
         if count%500 == 0:
             print "%d tweets inserted..." % count
         if count == int(sys.argv[1]):
